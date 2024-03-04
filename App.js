03440053fdcd69e707d411190f088c26a1c4b5e3
JormaByte <joel.oksanen1223@gmail.com>
2024-03-04 14:39:46 +0200

    components

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { Text, View } from 'react-native';
 import Styles from './styles/Styles'
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import List from './components/List';
+import AddWorkout from './components/AddWorkout';
+import Settings from './components/Settings';
+
+const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
-    <View>
-
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name='List' component={List}/>
+        <Tab.Screen name='Add Workout' component={AddWorkout}/>
+        <Tab.Screen name='Settings' component={Settings}/>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
---
+++ components/Settings.js
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+import Styles from './styles/Styles'
+
+export default function Settings() {
+  return (
+    <View>
+
+    </View>
+  );
+}
