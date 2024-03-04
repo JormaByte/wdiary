bf054e5a9da0577992aab636d143139dc21a6b92
JormaByte <joel.oksanen1223@gmail.com>
2024-03-04 14:31:13 +0200

    initial commit

---
+++ styles/Styles.js
@@ -0,0 +1,42 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container: {
+        flex: 0.8,
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        margin: 10,
+        border: 3
+    },
+    textInput:{
+        margin: 6,
+        padding: 10,
+        width: '85 %',
+        fontSize: 16,
+        borderWidth: 2,
+        borderRadius: 90,
+        borderColor: 'pink'
+    
+
+    },
+    label:{
+        fontSize: 18,
+        fontWeight: "bold"
+    },
+    
+    image:{
+        marginTop: 5
+    },
+    dropDown: {
+       
+    },
+    stats:{
+        fontSize: 18,
+        borderWidth: 2,
+        borderRadius: 90,
+        margin: 10,
+        padding: 10,
+        backgroundColor: 'bisque'
+    }
+});
+  
